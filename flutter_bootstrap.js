8dcad4898ea5fc2e8d842b9a18e1a1d343f8efdc
Reza HajRahimi <rezahajrahimi@yahoo.com>
2024-10-23 11:04:31 +0330

    fix add new admin

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2294385645"
+    serviceWorkerVersion: "2082223538"
   }
 });
